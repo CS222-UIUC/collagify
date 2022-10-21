ae0a2c7e039c5d5f7019b886a79bc2b284f3710c
quinnouyang <quinn.ouy@gmail.com>
2022-10-21 14:20:41 -0500

    Finished API authentication example, hiding client secret

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@chakra-ui/react";
+import { Button, Heading } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
