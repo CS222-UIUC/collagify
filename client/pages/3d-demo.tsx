88c87f60c3af803d111c28f6ef960e2035809274
Heaper327 <jameszhu327@outlook.com>
2022-11-03 01:42:15 -0500

    Remove redundant code from 3d-demo

--- client/pages/3d-demo.tsx
+++ client/pages/3d-demo.tsx
@@ -44,45 +44,33 @@ export default function Test3D() {
 }
 
 function Box(props) {
-    // This reference gives us direct access to the THREE.Mesh object
-    const ref = useRef(null)
-    // Hold state for hovered and clicked events
-    const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
-    // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((state, delta) => {
-        ref.current.rotation.x += 0.01;
-        ref.current.rotation.y += 0.01;
-    })
-    // Return the view, these are regular Threejs elements expressed in JSX
-    return (
-      <mesh
-        {...props}
-        ref={ref}
-        scale={1}
-        onPointerOver={(event) => hover(true)}
-        onPointerOut={(event) => hover(false)}>
-        <boxGeometry args={[3, 3, 3]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-    )
-  }
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef(null)
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => {
+      ref.current.rotation.x += 0.01;
+      ref.current.rotation.y += 0.01;
+  })
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={1}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[3, 3, 3]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
-  function Teapot(props) {
-    // This reference gives us direct access to the THREE.Mesh object
-    const ref = useRef(null)
-    // Hold state for hovered and clicked events
-    const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
-    // Subscribe this component to the render-loop, rotate the mesh every frame
-    // useFrame((state, delta) => {
-    //     ref.current.rotation.x += 0.01;
-    //     ref.current.rotation.y += 0.01;
-    // })
-    // Return the view, these are regular Threejs elements expressed in JSX
-    const model = useLoader(GLTFLoader, '/teapot/teapot.gltf')
-    return (
-        <primitive position={[0, 0.25, 0]} object={model.scene} />
-    )
-  }
+function Teapot(props) {
+  const model = useLoader(GLTFLoader, '/teapot/teapot.gltf')
+  return (
+      <primitive position={[0, 0.25, 0]} object={model.scene} />
+  )
+}
 
