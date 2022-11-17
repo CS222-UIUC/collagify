297d3799f19ca5f92fb69689825b11ca10b8453e
Heaper327 <jameszhu327@outlook.com>
2022-11-17 01:05:12 -0600

    Add navbar and chakra provider to all pages

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,31 @@
-import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import styles from "../styles/_app.module.css";
+import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme, Box } from "@chakra-ui/react";
+import Link from 'next/link';
 
-export default MyApp
+export default function App({ Component, pageProps }: AppProps) {
+
+  const theme = extendTheme({
+    colors: {
+      spotify: {
+        green: "#1DB954",
+        black: "#191414",
+        white: "#FFFFFF",
+      }
+    }
+  });
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box bgColor="spotify.black">
+        <header className={styles.navbar}>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+        </header>
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider>
+  );
+}
